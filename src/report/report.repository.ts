8f5be611fbecbc6885cf2c2bb35f56b6a37f4991
Thyago Lucas <salc@icomp.ufam.edu.br>
2024-03-05 13:41:06 -0400

    prisma service module

--- src/report/report.repository.ts
+++ src/report/report.repository.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { CreateReportDto } from './dto/create-report.dto';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
 export class ReportRepository {
-  findGiftCardSells(createReportDto: CreateReportDto) {
-    createReportDto;
-    return 'This action adds a new report';
+  constructor(private prisma: PrismaService) {}
+  async execute(id: number): Promise<any> {
+    const user_student = await this.prisma.student.findUnique({
+      where: { user_id: id },
+    });
+    return user_student;
   }
 }
